1b518c9baa8177d459d6b59dad36d356232b294f
guydoshlab <63221880+guydoshlab@users.noreply.github.com>
2025-12-15 14:51:18 -0500

    Add files via upload

---
+++ associated_files/caller_scripts_and_input_files/smorflist_caller.py
@@ -0,0 +1,38 @@
+import smorflist
+import sys
+
+# This caller script opens the txt input file, parses it, and for each rocc file specified, calls smorflist.
+# Pull in the name of the file with input params. Filename is only input for the caller.
+# Wrapper function for finding dORFs.	
+args1 = (sys.argv)
+if len(args1)!=2:
+	print("Error.")
+	exit()
+else:
+	f=open(args1[1])
+	inputparams=[line.strip() for line in f.readlines()] 
+	f.close()
+	tossedgenes=0
+
+	variables={}
+	nextlineisinput=0
+	for inputline in inputparams:
+		if inputline=="" or inputline[0]=="#":
+			continue
+		
+		if nextlineisinput!=0:
+			variables[nextlineisinput]=inputline
+			nextlineisinput=0
+		
+		if inputline[0:5]=="INPUT":
+		# The next line is an input line.
+			nextlineisinput=inputline.split("_")[-1]
+			
+	print("Variables collected by caller script:")
+	for key in (variables.keys()):
+		print(key)
+		print(variables[key])
+	print("\n--------------------------------\n")
+
+	# Now call the wrapper function
+	smorflist.main(variables["inputfiles"],variables["lengththresh"],variables["shift"],variables["smallest"],variables["mismatches"],variables["UTR"],variables["outfile"])
